5f1877fdb82baadc83a88c0a5ca077556b20175e
guidoriosciaffaroni <guidoriosciaffaroni@gmail.com>
2025-09-08 09:44:39 -0300

    2025_09_08_09_44_39

--- preinstall.sh
+++ preinstall.sh
@@ -1,12 +1,14 @@
-sudo apt update -y && sudo apt install -y dos2unix
+#sudo apt update -y && sudo apt install -y dos2unix
 
-dos2unix install.sh
-dos2unix apache_moodle.sh
+sudo apt install tmux
+
+#dos2unix install.sh
+#dos2unix apache_moodle.sh
 
 
-chmod +x install.sh
-chmod +x apache_moodle.sh
+#chmod +x install.sh
+#chmod +x apache_moodle.sh
 
-sudo ./install.sh
-sudo ./apache_moodle.sh
+#sudo ./install.sh
+#sudo ./apache_moodle.sh
 
