4c6428cd56ab704b0666341ea856f2e48e6c32b7
guidoriosciaffaroni <guidoriosciaffaroni@gmail.com>
2025-09-10 03:27:26 -0300

    2025_09_10_03_27_24

---
+++ 03_install_mariadb.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/
+
+echo ">>> Actualizando paquetes..."
+apt-get update -y
+apt-get upgrade -y
+
+echo ">>> Instalando MariaDB Server y Client..."
+apt-get install -y mariadb-server
+#apt-get install -y mariadb-client
+
+echo ">>> Habilitando y arrancando MariaDB..."
+#systemctl enable mariadb
+#systemctl start mariadb
+
+
+sudo apt autoremove -y
--- preinstall.sh
+++ preinstall.sh
@@ -3,8 +3,10 @@ chmod +x 00_install_app.sh
 chmod +x 01_install_apache2.sh
 chmod +x 02_install_php.sh
 chmod +x 03_install_mysql.sh
+#chmod +x 03_install_mariadb.sh
 
 sudo ./00_install_app.sh
 sudo ./01_install_apache2.sh
 sudo ./02_install_php.sh
 sudo ./03_install_mysql.sh
+#sudo ./03_install_mariadb.sh
